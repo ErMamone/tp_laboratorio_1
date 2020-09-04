c2c26bb9e2b551c8b357fe113498046f58d970b0
Eros <emamone@sysone.com>
2020-09-04 09:49:09 -0300

    Se agrega bibliotecas de operaciones (suma, resta, multiplicacion, division y factorizacion)

---
+++ Bibliotecas/funciones.c
@@ -0,0 +1,73 @@
+/*
+* Funcion de suma y
+* devuelve la suma de los dos primeros numeros dados
+*/
+
+unsigned int suma (int x, int y){
+    int sum = 0;
+
+    sum = x + y;
+
+    return sum;
+}
+
+/*
+* Funcion resta
+* devuelve la resta del primer con el segundo numero
+*/
+
+unsigned int resta (int x, int y){
+    int res = 0;
+
+    res = x - y;
+
+    return res;
+}
+/*
+* Funcion multiplicar
+* multiplica el primer por el segundo numero
+* sin importar el orden
+*/
+
+unsigned int multiplicar (int x, int y){
+    int multi = 0;
+
+    multi = x * y;
+
+    return multi;
+}
+
+/*
+* Funcion division
+* esta funcion divide el primer por el segundo numero
+* importa el orden para un resultado optimo
+*/
+
+float dividido (int x, int y){
+    float div = 0;
+
+	if(x<y){
+	   div = (float) y / x;
+	}else{
+	   div = (float) x / y;
+    }
+
+    return div;
+}
+
+/*
+* Funcion factorizar
+* esta factoriza los numeros dados
+*/
+
+unsigned int factorizar (int x){
+    int dev = 1;
+    int i;
+    int contadorN = x;
+    for(i=1; i<x; i++){
+        dev = dev * contadorN;
+        contadorN--;
+    }
+    return dev;
+}
+
---
+++ Bibliotecas/funciones.h
@@ -0,0 +1,9 @@
+unsigned int suma (int x, int y);
+
+unsigned int resta (int x, int y);
+
+float dividido (int x, int y);
+
+unsigned int multiplicar (int x, int y);
+
+unsigned int factorizar (int x);
